cf7081d040cb2ce9299d78e97436dea2868ca794
Eddie klaï <91609481+Eddieklai@users.noreply.github.com>
2025-07-14 16:48:49 +0200

    [add] implement Product management services with create, read, update, and delete functionalities

---
+++ src/services/productServices.tsx
@@ -0,0 +1,62 @@
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_API_URL_LOCAL;
+
+export const createProduct = async (name: string, price:number) => {
+    try {
+        const response = await axios.post(`${API_URL}/product`, {
+            name,
+            price,
+        });
+        if (response.status !== 201) {
+            throw new Error('Product creation failed');
+        }
+        console.log('Product created successfully:', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Product creation failed:', error);
+        throw error;
+    }
+}
+
+export const getProducts = async () => {
+    try {
+        const response = await axios.get(`${API_URL}/product`);
+        if (response.status !== 200) {
+            throw new Error('Failed to fetch products');
+        }
+        console.log('Products fetched successfully:', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Failed to fetch products:', error);
+        throw error;
+    }
+}
+
+export const updateProduct = async (id: string, data: { name?: string; price?: number }) => {
+    try {
+        const response = await axios.put(`${API_URL}/product/${id}`, data);
+        if (response.status !== 200) {
+            throw new Error('Product update failed');
+        }
+        console.log('Product updated successfully:', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Product update failed:', error);
+        throw error;
+    }
+}
+
+export const deleteProduct = async (id: string) => {
+    try {
+        const response = await axios.delete(`${API_URL}/product/${id}`);
+        if (response.status !== 200) {
+            throw new Error('Product deletion failed');
+        }
+        console.log('Product deleted successfully:', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Product deletion failed:', error);
+        throw error;
+    }
+}
