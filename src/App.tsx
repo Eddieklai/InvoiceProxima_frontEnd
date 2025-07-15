dadb267e6d2f7a417d3fda90aa03df22368eeeaf
Eddie klaï <91609481+Eddieklai@users.noreply.github.com>
2025-07-15 11:36:02 +0200

    [add] implement routing and layout for invoices, products, clients, and settings pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,13 @@ import Login from '@/pages/Login';
 import Register from '@/pages/Register';
 import Dashboard from '@/pages/Dashboard';
 
+import Invoices from '@/pages/Invoices';
+import Products from '@/pages/Products';
+import Clients from '@/pages/Clients';
+import Settings from '@/pages/Settings';
+
+import PrivateLayout from './components/Layout/PrivateLayout';
+
 import { AuthProvider } from '@/context/AuthContext';
 
 function App() {
@@ -22,7 +29,39 @@ function App() {
             <PrivateRoute>
               <Dashboard />
             </PrivateRoute>
-          }/>
+          } />
+
+          <Route path="/invoices" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <Invoices />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
+
+          <Route path="/products" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <Products />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
+
+          <Route path="/clients" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <Clients />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
+
+          <Route path="/settings" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <Settings />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
           <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
